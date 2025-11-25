050c4271348226289024f2c9108b27d4b9e9da21
jai-git4208 <jaiminpansal@gmail.com>
2025-11-25 17:01:38 +0530

    implement boot sequence to display "I LOVE AVISHKA" on VGA

--- src/impl/x86_64/boot/main.asm
+++ src/impl/x86_64/boot/main.asm
@@ -1,9 +1,41 @@
 global start
-extern long_mode_start
-
 section .text
 bits 32
+
 start:
-	;print "ok"
-    mov dword [0xb8000], 0x2f4b2f4f
+    mov edi, 0xb8000     ; VGA text buffer
+
+    mov ax, 0x2F49       ; 'I'
+    stosw
+    mov ax, 0x2F20       ; ' '
+    stosw
+    mov ax, 0x2F4C       ; 'L'
+    stosw
+    mov ax, 0x2F4F       ; 'O'
+    stosw
+    mov ax, 0x2F56       ; 'V'
+    stosw
+    mov ax, 0x2F45       ; 'E'
+    stosw
+    mov ax, 0x2F20       ; ' '
+    stosw
+    mov ax, 0x2F41       ; 'A'
+    stosw
+    mov ax, 0x2F56       ; 'V'
+    stosw
+    mov ax, 0x2F49       ; 'I'
+    stosw
+    mov ax, 0x2F53       ; 'S'
+    stosw
+    mov ax, 0x2F48       ; 'H'
+    stosw
+    mov ax, 0x2F4B       ; 'K'
+    stosw
+    mov ax, 0x2F41       ; 'A'
+    stosw
+    mov ax, 0x2F7E       ; '~'
+    stosw
+    mov ax, 0x2F7E       ; '~'
+    stosw
+
     hlt
